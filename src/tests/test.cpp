e9d0560cb41fb3490b9f5b204ffafbb2bf3c3684
Charly <1152671+cmourglia@users.noreply.github.com>
2024-05-05 16:53:55 +0200

    Add a multiplication test
    Tests ADC, STX, STY, LDA, BNE

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -7,6 +7,8 @@ TEST_CASE("a9_lda_immediate_load_data", "[opcodes][lda]") {
     CPU cpu = init_cpu({0xa9, 0x05, 0x00});
     run_cpu(&cpu);
 
+    fprintf(stderr, "Status: 0x%x (0x%x)\n", cpu.status, 0b00110000);
+
     REQUIRE(cpu.accumulator == 0x05);
     REQUIRE(cpu.status == (BreakCommand | UnusedFlag));
 }
@@ -80,4 +82,21 @@ TEST_CASE("simple_program_1", "[programs]") {
     REQUIRE(cpu.x == 0xc1);
     REQUIRE(cpu.y == 0);
     REQUIRE(cpu.status == (BreakCommand | UnusedFlag | NegativeFlag));
+}
+
+TEST_CASE("10 times 3", "[programs]") {
+    std::vector<u8> prg = {0xa2, 0x0a, 0x8e, 0x00, 0x00, 0xa2, 0x03, 0x8e,
+                           0x01, 0x00, 0xac, 0x00, 0x00, 0xa9, 0x00, 0x18,
+                           0x6d, 0x01, 0x00, 0x88, 0xd0, 0xfa, 0x8d, 0x02,
+                           0x00, 0xea, 0xea, 0xea, 0x00};
+
+    CPU cpu = init_cpu(prg);
+
+    run_cpu(&cpu);
+
+    // REQUIRE(cpu.accumulator == 30);
+    // REQUIRE(cpu.x == 3);
+    // REQUIRE(cpu.y == 0);
+
+    REQUIRE(cpu.memory[2] == 30);
 }
