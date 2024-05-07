e09100aff76ac982360908137ee2aff604c4fce0
Charly <1152671+cmourglia@users.noreply.github.com>
2024-05-08 00:30:06 +0200

    Run nestest as a test

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "bus.h"
+#include "cartridge.h"
 #include "cpu.h"
-#include "disassembler.h"
 
 #if 0
 TEST_CASE("a9_lda_immediate_load_data", "[opcodes][lda]") {
@@ -102,3 +103,32 @@ TEST_CASE("10 times 3", "[programs]") {
     REQUIRE(cpu.memory[2] == 30);
 }
 #endif
+
+TEST_CASE("NES TEST", "[nestest]") {
+    Cartridge cartridge = load_cartridge("test_roms/nestest.nes");
+    defer(release_cartridge(&cartridge));
+
+    REQUIRE(cartridge.prg_rom_size == 0x4000);
+    REQUIRE(cartridge.prg_rom != nullptr);
+    REQUIRE(cartridge.chr_rom_size == 0x2000);
+    REQUIRE(cartridge.chr_rom != nullptr);
+
+    Bus bus = {};
+    bus_load_cartridge(&bus, &cartridge);
+
+    bus_reset(&bus);
+
+    // We need this for automation
+    bus.cpu.program_counter = 0xC000;
+
+    run_cpu(&bus.cpu);
+
+    REQUIRE(read_mem(&bus, 0x02) == 0);
+    REQUIRE(read_mem(&bus, 0x03) == 0);
+
+    REQUIRE(bus.cpu.program_counter != 0xC000);
+    REQUIRE(bus.cpu.accumulator == 0xB9);
+    REQUIRE(bus.cpu.x == 0xFF);
+    REQUIRE(bus.cpu.y == 0x15);
+    REQUIRE(bus.cpu.status == 0xB4);
+}
